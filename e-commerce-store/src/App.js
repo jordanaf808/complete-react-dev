3b881617230161398676b53ffaae9570f452d3a9
Jordan Aceves-Foster <69128716+jordanaf808@users.noreply.github.com>
2020-12-01 17:23:11 -0800

    add header component

--- e-commerce-store/src/App.js
+++ e-commerce-store/src/App.js
@@ -5,20 +5,18 @@ import './App.scss';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats</h1>
-  </div>
-);
+import Header from './components/header/header.component';
 
 function App() {
-  return <div>
-    <switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </switch>
-    </div>;
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  ); 
 }
 
 export default App;
