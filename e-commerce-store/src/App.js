ca76b90c5c28e50b0e0d5094f8f2f1542ce3552f
Jordan Aceves-Foster <69128716+jordanaf808@users.noreply.github.com>
2020-12-03 11:15:14 -0800

    add firebase google auth

--- e-commerce-store/src/App.js
+++ e-commerce-store/src/App.js
@@ -7,41 +7,37 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-// For our app to authenticate the Current User we need to
-// pass auth through the State so our components have access
-// to that information.
+// We need to store the State of our user in the App. firebase {auth} needs 
+// to have access to the State so it can pass that information to all of our 
+// components that need it.
 
-// To pass state to our components we need to change this to a
-// Class Component.
+// To store state in that App we need to change it to a 'class' component:
 // function App() {
 //   return (
 class App extends React.Component {
   constructor() {
     super();
-
     this.state = {
       currentUser: null,
     };
   }
-
   // must close auth when app closes, so we don't have any memory leaks
   // init a unsubscribe method set to null by default
   unsubscribeFromAuth = null;
   // pass in the current user to the unsubscribe method
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async(user) => {
+      createUserProfileDocument(user);
+      // this.setState({ currentUser: user });
       console.log(user);
     });
   }
-
   // when this lifecycle is called, you will be unsubscribed from auth.
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-
   render() {
     return (
       <div>
