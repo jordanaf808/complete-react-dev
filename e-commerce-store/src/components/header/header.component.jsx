daba03661a6e995a6456035c988973016a661a2b
Jordan Aceves-Foster <69128716+jordanaf808@users.noreply.github.com>
2020-12-07 14:50:32 -0800

    added cartIcon and cartDropdown components and cart reducer to our app

--- e-commerce-store/src/components/header/header.component.jsx
+++ e-commerce-store/src/components/header/header.component.jsx
@@ -1,41 +1,47 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux'; // connects your component to redux 
+import { connect } from "react-redux"; // connects your component to redux
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo className="logo" />
+const Header = ({ currentUser, hidden }) => (
+  <div className='header'>
+    <Link to='/' className='logo-container'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    <div className='options'>
+      <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className="option" to="/shop">
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+        <div className='option' onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="/signin">
+        <Link className='option' to='/signin'>
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 // returns state which is the 'root reducer'. navigate to the value desired:
 // root reducer(state)=> user reducer(user) => property(currentUser)
-const  mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
 // 'connect': is a higher order component. they are just functions that take components as arguments
 // and return a new 'souped-up' component.
 
