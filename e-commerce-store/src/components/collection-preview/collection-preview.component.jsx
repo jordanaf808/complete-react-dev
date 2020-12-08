f2144755d00bb2844adf87b0f130e434bc69c05c
Jordan Aceves-Foster <69128716+jordanaf808@users.noreply.github.com>
2020-12-07 16:48:31 -0800

    add Cart Item Redux action, and integrate

--- e-commerce-store/src/components/collection-preview/collection-preview.component.jsx
+++ e-commerce-store/src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import CollectionItem from '../collection-item/collection-item.component';
+import CollectionItem from "../collection-item/collection-item.component";
 
-import './collection-preview.styles.scss';
+import "./collection-preview.styles.scss";
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {items
         .filter((item, index) => index < 4)
-        .map(({id, ...otherItemProps}) => (
-        <CollectionItem key={id} {...otherItemProps} />
-      ))}
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
 );
--- e-commerce-store/src/redux/cart/cart.reducer.js
+++ e-commerce-store/src/redux/cart/cart.reducer.js
@@ -1,17 +1,31 @@
-import CartActionTypes from './cart.types';
+import CartActionTypes from "./cart.types";
 
+// add value of cartItems into our initial-state of our reducer
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: [],
 };
 
+// create cart.action to get item type and payload.
+// Add this action 'case' to our cart-reducer to listen for.
+// When called it will return the existing state, and the cartItems
+// array, where we spread the existing items and add the new item(s)
+// payload to it.
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden: !state.hidden
+        hidden: !state.hidden,
       };
-      default: 
+    //
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        // combine old cart items with new cart-items in this payload.
+        cartItems: [...state.cartItems, action.payload],
+      };
+    default:
       return state;
   }
 };
