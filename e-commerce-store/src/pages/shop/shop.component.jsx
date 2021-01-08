4de471912f20f91aed77f8f52f59352f7bbf5eb8
Jordan Aceves-Foster <69128716+jordanaf808@users.noreply.github.com>
2021-01-08 15:25:15 -0800

    fixed a bug when reloading shop page and shop data would not load

--- e-commerce-store/src/pages/shop/shop.component.jsx
+++ e-commerce-store/src/pages/shop/shop.component.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
+// import {
+//   firestore,
+//   convertCollectionsSnapshotToMap,
+// } from '../../firebase/firebase.utils';
+
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+// selector for isLoading info
 import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from '../../firebase/firebase.utils';
-
-import { updateCollections } from '../../redux/shop/shop.actions';
+  selectIsCollectionFetching,
+  selectIsCollectionsLoaded,
+} from '../../redux/shop/shop.selectors';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
@@ -35,53 +41,64 @@ class ShopPage extends React.Component {
   //     loading: true,
   //   };
   // }
-  // we can shorten this with newer syntax
-  state = {
-    loading: true,
-  };
-  unsubscribeFromSnapshot = null;
+  // we can shorten this with newer syntax *no longer need because we moved loading to our Thunk function.
+  // state = {
+  //   loading: true,
+  // };
+  // unsubscribeFromSnapshot = null;
   componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection('collections'); // reference to collection we named 'collections'
-    //   this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-    //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-    //     updateCollections(collectionsMap);
-    //     this.setState({ loading: false }); // deactivate spinner by setting loading to false.
-    //   });
-    //  We are using an observer/subscription pattern we get with firestore that gives us a snapshot any time the db is modified. If 'observable' object is unavailable we can change it to a promise based function, instead of .onSnapshot.
-    // .get() firestore function that returns a promise with all document(s) from a collection. It returns a promise with a snapshot object just like we got before. However we do not get a continuous subscription like we do with our observer pattern above, only when component mounts.
-    collectionRef.get().then(snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false }); // deactivate spinner by setting loading to false.
-    });
-    // Using a regular Fetch API call instead of our previous methods. However the data returned is not in the right pattern
-    // fetch(
-    //   'https://firestore.googleapis.com/v1/projects/e-commerce-e56a3/databases/(default)/documents/collections'
-    // )
-    //   .then(response => response.json())
-    //   .then(collections => console.log(collections));
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync(); // Async Thunk method to kick off multiple actions. To help reducer with async state props and getting collections from Firebase.
   }
+  //  *** removed for Thunk stuff ***
+  //   const { updateCollections } = this.props;
+  //   const collectionRef = firestore.collection('collections'); // reference to collection we named 'collections'
+  //   //   this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+  //   //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+  //   //     updateCollections(collectionsMap);
+  //   //     this.setState({ loading: false }); // deactivate spinner by setting loading to false.
+  //   //   });
+  //   //  We are using an observer/subscription pattern we get with firestore that gives us a snapshot any time the db is modified. If 'observable' object is unavailable we can change it to a promise based function, instead of .onSnapshot.
+  //   // .get() firestore function that returns a promise with all document(s) from a collection. It returns a promise with a snapshot object just like we got before. However we do not get a continuous subscription like we do with our observer pattern above, only when component mounts.
+  //   collectionRef.get().then(snapshot => {
+  //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+  //     updateCollections(collectionsMap);
+  //     this.setState({ loading: false }); // deactivate spinner by setting loading to false.
+  //   });
+  //   // Using a regular Fetch API call instead of our previous methods. However the data returned is not in the right pattern
+  //   // fetch(
+  //   //   'https://firestore.googleapis.com/v1/projects/e-commerce-e56a3/databases/(default)/documents/collections'
+  //   // )
+  //   //   .then(response => response.json())
+  //   //   .then(collections => console.log(collections));
+  // }
 
-  // to use our new wrapped components we need to use the render={} method in our <Route />
-  // we also need to send the match object data (e.g. history, location) that we use inside of our <CollectionPage/> component
-  // and make sure we pass it through our new spinner wrappers as 'props'
+  // // to use our new wrapped components we need to use the render={} method in our <Route />
+  // // we also need to send the match object data (e.g. history, location) that we use inside of our <CollectionPage/> component
+  // // and make sure we pass it through our new spinner wrappers as 'props'
   render() {
-    const { match } = this.props;
-    const { loading } = this.state;
+    const { match, isCollectionFetching, areCollectionsLoaded } = this.props;
+    // const { loading } = this.state; no longer need because Thunk. instead of fetching isLoading from the state,
+    // we can now replace isLoading with our async Thunk: isCollectionFetching passed from our state to our props
     return (
       <div className='shop-page'>
         <Route
           exact
           path={`${match.path}`}
           render={props => (
-            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+            <CollectionsOverviewWithSpinner
+              isLoading={isCollectionFetching}
+              {...props}
+            />
           )}
         />
         <Route
           path={`${match.path}/:collectionId`}
           render={props => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
+            <CollectionPageWithSpinner
+              isLoading={!areCollectionsLoaded}
+              {...props}
+            />
           )}
         />
       </div>
@@ -89,9 +106,21 @@ class ShopPage extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  updateCollections: collectionsMap =>
-    dispatch(updateCollections(collectionsMap)),
+// When we refresh a shop collection page, the app will break because when our collection component is loading it expects our collection data to be loaded already. However because React renders our components before calling the 'ComponentDidMount' lifecycle the collection has not been returned yet. Our isLoading value is initially set to false, which in turn renders a missing collection object.
+//  To remedy: instead of checking whether collection is fetching, we need to check whether our collection has loaded, by making another selector which checks whether our collection is !!falsey: e.g.null or not.
+// Now if 'isLoading' is true it will render the spinner, if false it will render the wrapped component. If our collection Has loaded our new areCollectionsLoaded selector returns true, it will trigger our loading spinner instead of loading the collection page, so we must switch the return value with '!'
+
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching,
+  areCollectionsLoaded: selectIsCollectionsLoaded,
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+// const mapDispatchToProps = dispatch => ({
+//   updateCollections: collectionsMap =>
+//     dispatch(updateCollections(collectionsMap)),
+// });
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
